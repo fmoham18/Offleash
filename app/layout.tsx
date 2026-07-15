170d0d894da038d5705a0a0d2e7337c7c5517fcf
huamanie <huamaniedwin@gmail.com>
2026-07-15 00:51:16 -0400

    Added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import localFont from 'next/font/local'
+import OL_Logo from '../public/OL_Logo_not_transparent.png'
 
 const edbert = localFont({
   src: '../app/fonts/Edbert-Regular.otf',
@@ -11,7 +12,10 @@ const edbert = localFont({
 export const metadata: Metadata = {
   title: "Offleash",
   description: "The offical website for Offleash, a band based in the DMV area. Check out our music, socials, and upcoming shows!",
-};
+  icons: {
+    icon: OL_Logo.src,
+  },
+}
 
 export default function RootLayout({
   children,
@@ -19,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`scroll-smooth font-serif ${edbert.variable}`}>
+    <html lang="en" className={`scroll-smooth font-serif ${edbert.variable}`} >
       <body className="min-h-full">{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -9,5 +9,5 @@ export default function Main() {
       <MainPage />
       <SecondPage />
     </div>
-  );
+  )
 }
